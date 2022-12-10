b0aba34037eaba16643a3b0b1c54aeefbaa03e63
yusif Shawish <yusifshawish@orefresh.io>
2022-12-10 22:17:38 +0300

    something

---
+++ Calendar.js
@@ -0,0 +1,60 @@
+document.getElementById("home").onclick = function() {
+    window.location.href = "minigame.html"}
+const currentDate = document.querySelector(".currentDate"),
+daysId = document.querySelector(".days"),
+iconBeforeNext = document.querySelectorAll(".icons span");
+ 
+let date = new Date();
+currYear = date.getFullYear();
+currMonth = date.getMonth();
+ 
+const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"]
+ 
+iconBeforeNext.forEach(icon =>
+{
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "next" ? currMonth + 1 : currMonth - 1;
+        if (currMonth < 0 || currMonth > 11)
+        {
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        }
+        else
+        {
+            date = new Date();
+        }
+ 
+        renderCalendar();
+    });
+});
+ 
+const renderCalendar = () =>
+{
+    let firstDayOfMon = new Date(currYear, currMonth, 1).getDay(),
+    lstDateOfMon = new Date(currYear, currMonth + 1, 0).getDate(),
+    lstDayOfMon = new Date(currYear, currMonth, lstDateOfMon).getDay(),
+    lstDateoflstMon = new Date(currYear, currMonth, 0).getDate();
+    let listTag = "";
+ 
+    for (let i = firstDayOfMon; i > 0; i--)
+    {
+        listTag += `<li class="inactive">${lstDateoflstMon - i + 1}</li>`;
+    }
+ 
+    for (let i = 1; i <= lstDateOfMon; i++)
+    {
+        let today = i === date.getDate() && currMonth == date.getMonth() && currYear == date.getFullYear() ? "active" : "";
+        listTag += `<li class="${today}">${i}</li>`;
+    }
+ 
+    for(let i = lstDayOfMon; i < 6; i++)
+    {
+        listTag += `<li class="inactive">${i - lstDayOfMon + 1}</li>`;
+    }
+   
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysId.innerHTML = listTag;
+}
+ 
+renderCalendar();
