133764eb6b31961a171290239aefddd2154a14b8
Fares-Lutfi <FaresLutfi24@kingsacademy.edu.jo>
2022-12-11 08:15:39 +0300

    dvv

--- Calendar.js
+++ Calendar.js
@@ -1,15 +1,15 @@
-document.getElementById("home").onclick = function() {
-    window.location.href = "minigame.html"}
+// document.getElementById("home").onclick = function() {
+//     window.location.href = "minigame.html"}
 const currentDate = document.querySelector(".currentDate"),
 daysId = document.querySelector(".days"),
 iconBeforeNext = document.querySelectorAll(".icons span");
- 
+    
 let date = new Date();
 currYear = date.getFullYear();
 currMonth = date.getMonth();
- 
-const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"]
- 
+    
+const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"];
+    
 iconBeforeNext.forEach(icon =>
 {
     icon.addEventListener("click", () => {
@@ -24,11 +24,11 @@ iconBeforeNext.forEach(icon =>
         {
             date = new Date();
         }
- 
+    
         renderCalendar();
     });
 });
- 
+    
 const renderCalendar = () =>
 {
     let firstDayOfMon = new Date(currYear, currMonth, 1).getDay(),
@@ -36,25 +36,26 @@ const renderCalendar = () =>
     lstDayOfMon = new Date(currYear, currMonth, lstDateOfMon).getDay(),
     lstDateoflstMon = new Date(currYear, currMonth, 0).getDate();
     let listTag = "";
- 
+    
     for (let i = firstDayOfMon; i > 0; i--)
     {
         listTag += `<li class="inactive">${lstDateoflstMon - i + 1}</li>`;
     }
- 
+    
     for (let i = 1; i <= lstDateOfMon; i++)
     {
-        let today = i === date.getDate() && currMonth == date.getMonth() && currYear == date.getFullYear() ? "active" : "";
+        let today = i === new Date().getDate() && currMonth == new Date().getMonth() && currYear == new Date().getFullYear() ? "active" : "";
         listTag += `<li class="${today}">${i}</li>`;
     }
- 
+    
     for(let i = lstDayOfMon; i < 6; i++)
     {
         listTag += `<li class="inactive">${i - lstDayOfMon + 1}</li>`;
     }
-   
+    
+    console.log(listTag);
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysId.innerHTML = listTag;
 }
- 
+    
 renderCalendar();
--- todo.js
+++ todo.js
@@ -1,8 +1,10 @@
 const todos = JSON.parse(localStorage.getItem('todo')) || [];
 const list = document.getElementsByClassName('list')[0];
 const todo = document.getElementsByClassName('todo')[0];
+const dueDate = document.getElementsByClassName('todo')[1];
 const addBtn = document.getElementsByClassName('add')[0];
 const delAllBtn = document.getElementsByClassName('danger')[0];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Novr", "Dec"];
 
 todo.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
@@ -13,8 +15,50 @@ addBtn.addEventListener('click', addTodo);
 list.addEventListener('click', delTodo)
 delAllBtn.addEventListener('click', delAllTodo);
 
+function countdown(date)
+{
+  var due = new Date(date).getTime();
+  var diff;
+
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = due - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    diff = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      distance = now - due;
+      days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      diff = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    }
+
+    return diff;
+  }, 1000);
+}
+
+function breakDownDate()
+{
+  dueYear = dueDate.value.substring(0,4);
+  dueMon = dueDate.value.substring(5,7);
+  dueDay = dueDate.value.substring(8,10);
+  dueHour = dueDate.value.substring(11,13);
+  dueMin = dueDate.value.substring(14,16);
+  dueMonth = months[dueMon - 1];
+  date = dueMonth + " " + dueDay + ", " + dueYear + " " + dueHour + ":" + dueMin + ":59";
+  return date;
+}
+
 function addTodo() {
-  const newTodo = todo.value.trim();
+  
+  const newTodo = todo.value.trim() + " Due: " + breakDownDate() + " Countdown: ";
   if (newTodo == '') { return };
   todos.push(newTodo);
   todo.value = '';
@@ -43,7 +87,7 @@ function saveTodos() {
 function updateTodos() {
   let str = '';
   for (let i = 0; i < todos.length; i++) {
-    str += `<li>${todos[i]}<a href="#" data-num="${i}">Delete</a></li>`
+    str += `<li>Task: ${todos[i]}<a href="#" data-num="${i}">Delete</a></li>`
   }
   list.innerHTML = str;
   if (todos.length !== 0) {
